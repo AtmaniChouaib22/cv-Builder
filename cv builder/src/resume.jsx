7f560f15655e0c19e168d6a4f65ecbb25931edc2
Atmani Chouaib <atmanichouaib22@gmail.com>
2023-11-24 21:39:35 +0100

    added resume paper styling

--- cv builder/src/resume.jsx
+++ cv builder/src/resume.jsx
@@ -1,13 +1,16 @@
 import DisplayPersonal from "./displayPersonal"
 import DisplayEducation from "./displayEducation";
 import DisplayExperience from "./displayExperience";
+import './styles/resume.css'
 
 function CreateResume({submitted, eduSubmitted, expSubmitted, personalInfo, educationInfo, experienceInfo}) {
     return (
         <div className="Resume">
-             {submitted && <DisplayPersonal personalInfo={personalInfo}/>}
-             {eduSubmitted && <DisplayEducation educationInfo={educationInfo} />}
-             {expSubmitted && <DisplayExperience experienceInfo={experienceInfo} />}
+             <div className="Resume-items">
+                {submitted && <DisplayPersonal personalInfo={personalInfo}/>}
+                {eduSubmitted && <DisplayEducation educationInfo={educationInfo} />}
+                {expSubmitted && <DisplayExperience experienceInfo={experienceInfo} />}
+             </div>
         </div>
     )
 }
